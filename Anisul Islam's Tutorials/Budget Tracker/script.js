af75c3dd7e106e6aecb257f111fcbc5cd49f3721
julfikar092 <julfikar.iitdu@gmai.com>
2026-02-19 23:33:18 +0600

    New Tutorials of Anisul Islam

---
+++ Anisul Islam's Tutorials/Budget Tracker/script.js
@@ -0,0 +1,22 @@
+/* console.log("Budget Tracker App")
+
+// Phase 1:
+console.log("Name: Mohammad Julfikar Rahman")
+console.log("Salary: ", 42000)
+console.log("Expenses: ", 42000)
+
+// Phase 2:
+let name = "Mohammad Julfikar Rahman"
+let income = 42000
+let expences = 38000
+console.log("Name: ", name, "\nIncome: ", income, "\nExpenses: ", expences)
+ */
+
+// Phase 3:
+
+let name1 = "Mohammad Julfikar Rahman"
+let income1= 42000
+let expences1 = 38000
+document.write("User: "+ name1.toUpperCase())
+document.write("<br>Total Income: " + "$"+income1)
+document.write("<br>Total Expenses: " + "$"+expences1)
